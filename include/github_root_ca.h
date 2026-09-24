7e3ce6f2ef65a9c2352146dafcf5e8cd0baa8527
User <user@example.com>
2026-09-23 20:00:18 -0700

    Update include directory

--- include/github_root_ca.h
+++ include/github_root_ca.h
@@ -1,20 +1,30 @@
 #pragma once
 
-/* ---------------------------------------------------------------------------
- * DigiCert Global Root G2 – The Root Certificate Authority (CA) used to
- * verify TLS/HTTPS connections to GitHub Pages (*.github.io).
- * --------------------------------------------------------------------------- */
+/*
+ * DigiCert Global Root G2
+ * Valid until: January 15, 2038
+ */
 
-static const char GITHUB_ROOT_CA[] = 
+static const char GITHUB_ROOT_CA[] =
 "-----BEGIN CERTIFICATE-----\n"
-"MIIDjjCCAnagAwIBAgIQAzRxOAgACQAAAAAAAGn3ACANBgkqhkiG9w0BAQsFADBh\n"
+"MIIDjjCCAnagAwIBAgIQAzrx5qcRqaC7KGSxHQn65TANBgkqhkiG9w0BAQsFADBh\n"
 "MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n"
-"d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExREaWdpQ2VydCBHbG9iYWwgUm9vdCBH\n"
+"d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBH\n"
 "MjAeFw0xMzA4MDExMjAwMDBaFw0zODAxMTUxMjAwMDBaMGExCzAJBgNVBAYTAlVT\n"
 "MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j\n"
-"b20xIDAeBgNVBAMTFERpZ2lDZXJ0IEdsb2JhbCBSb290IEdSMIIBIjANBgkqhkiG\n"
-"9w0BAQEFAAOCAQ8AMIIBCgKCAQEAu1fg2I3IAnP98NnyxS4p3A4GqIe1wE25A94f\n"
-"06x6T8kO/m7M4TzI2eC3fI1Z4b4X+3sF4d6lH7W/Lq4vU9l6j+nUqK2X9N7S9K+Y\n"
-"s7R02j3n/8d8J+U3dY4vN6q7yB4R8e+YpI3nS6A/uY1E2k4E12eS1/LqX9O616t1\n"
-"M8I7Vv3yO1l6K7X9a+b4K9x1r1n2o+p4N9K8X7N6s2+O5b9v3Y+8L4K9O1a8\n"
+"b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IEcyMIIBIjANBgkqhkiG\n"
+"9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuzfNNNx7a8myaJCtSnX/RrohCgiN9RlUyfuI\n"
+"2/Ou8jqJkTx65qsGGmvPrC3oXgkkRLpimn7Wo6h+4FR1IAWsULecYxpsMNzaHxmx\n"
+"1x7e/dfgy5SDN67sH0NO3Xss0r0upS/kqbitOtSZpLYl6ZtrAGCSYP9PIUkY92eQ\n"
+"q2EGnI/yuum06ZIya7XzV+hdG82MHauVBJVJ8zUtluNJbd134/tJS7SsVQepj5Wz\n"
+"tCO7TG1F8PapspUwtP1MVYwnSlcUfIKdzXOS0xZKBgyMUNGPHgm+F6HmIcr9g+UQ\n"
+"vIOlCsRnKPZzFBQ9RnbDhxSJITRNrw9FDKZJobq7nMWxM4MphQIDAQABo0IwQDAP\n"
+"BgNVHRMBAf8EBTADAQH/MA4GA1UdDwEB/wQEAwIBhjAdBgNVHQ4EFgQUTiJUIBiV\n"
+"5uNu5g/6+rkS7QYXjzkwDQYJKoZIhvcNAQELBQADggEBAGBnKJRvDkhj6zHd6mcY\n"
+"1Yl9PMWLSn/pvtsrF9+wX3N3KjITOYFnQoQj8kVnNeyIv/iPsGEMNKSuIEyExtv4\n"
+"NeF22d+mQrvHRAiGfzZ0JFrabA0UWTW98kndth/Jsw1HKj2ZL7tcu7XUIOGZX1NG\n"
+"Fdtom/DzMNU+MeKNhJ7jitralj41E6Vf8PlwUHBHQRFXGU7Aj64GxJUTFy8bJZ91\n"
+"8rGOmaFvE7FBcf6IKshPECBV1/MUReXgRPTqh5Uykw7+U0b6LJ3/iyK5S9kJRaTe\n"
+"pLiaWN0bfVKfjllDiIGknibVb63dDcY3fe0Dkhvld1927jyNxF1WW6LZZm6zNTfl\n"
+"MrY=\n"
 "-----END CERTIFICATE-----\n";
